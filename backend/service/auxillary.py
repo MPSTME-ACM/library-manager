8c68b754a23b9785efb698e525a87f92fb40840e
Parth Acharya <1008parth@gmail.com>
2024-12-31 13:21:06 +0530

    Fixes missing arg for @wraps()

--- backend/service/auxillary.py
+++ backend/service/auxillary.py
@@ -1,11 +1,11 @@
 '''Helper functions and decorators'''
 from functools import wraps
-from flask import request, current_app
+from flask import request
 from werkzeug.exceptions import BadRequest
 
 ### Decorators ###
 def enforce_JSON(endpoint):
-    @wraps
+    @wraps(endpoint)
     def decorated(*args, **kwargs):
         if request.mimetype.split("/")[-1].lower() != "json":
             raise BadRequest(f"{request.method} {request.root_path} expects JSON body")
