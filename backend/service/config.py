1f765ef437c6a58570d09aee8d3f89f14e260e8e
Parth Acharya <1008parth@gmail.com>
2024-12-31 13:32:18 +0530

    Adds new variable MAX_QLEN to enforce max no. of parties who can wait for a slot

--- backend/service/config.py
+++ backend/service/config.py
@@ -28,6 +28,7 @@ class AppConfig:
         OPENIING_TIME = int(os.environ["LIB_OPENING_TIME"])
         CLOSING_TIME = int(os.environ["LIB_CLOSING_TIME"])
         FUTURE_WINDOW_SIZE = int(os.environ["LIB_FUTURE_WINDOW_SIZE"])
+        MAX_QLEN = int(os.environ["LIB_MAX_QUEUE_SIZE"])
 
     except KeyError as e:
         print(f"ERROR: Missing configuration in {os.path.dirname(CWD), '.env'}, check .env file, Original Error: {e}")
